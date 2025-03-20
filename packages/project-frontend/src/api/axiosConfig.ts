f104ea293a43cbe64db7a87e8d2b103d71958e98
alexbanham <alexbanham2016@gmail.com>
2025-03-19 19:45:10 -0700

    Final Project - Updated API baseURL to point at VPS

--- packages/project-frontend/src/api/axiosConfig.ts
+++ packages/project-frontend/src/api/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://abanham.csse.dev/',
 });
 
 // Add an interceptor to attach the Authorization header if a token exists.
